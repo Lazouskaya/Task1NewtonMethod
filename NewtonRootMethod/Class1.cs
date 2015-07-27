b9764a5a75638ee69b32cd27cef845cbc109bc06
Viktoryia Lazouskaya <Viktoryia_Lazouskaya@epam.com>
2015-07-27 13:21:16 +0300

    add task1

---
+++ NewtonRootMethod/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NewtonRootMethod
+{
+    public static class NewtonsMethod
+    {
+        public static double RootOfNthDegreeByNewton(double number, int power, double accuracy)
+        {
+            if (number < 0 && power % 2 == 0)
+            {
+                throw new System.ArgumentException("Invalid value of number or power");
+            }
+            if (power < 0)
+            {
+                throw new System.ArgumentException("Invalid value of power. It must be greater than 0");
+            }
+            if (accuracy < 0 || accuracy > 1)
+            {
+                throw new System.ArgumentException("Invalid value of power. It must be in [0;1] interval");
+            }
+            double root;
+            double newroot = number;
+            do
+            {
+                root = newroot;
+                newroot = ((power - 1) * root + number / Math.Pow(root, (power - 1))) / power;
+            } while (newroot / root < accuracy);
+            return newroot;
+        }
+    }
+}
---
+++ TestRootByNewton/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NewtonRootMethod;
+
+namespace TestRootByNewton
+{
+    [TestClass]
+    public class NewtonMethodTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void NewtonTestPossitive()
+        {
+            Random rnd = new Random();
+            double number = rnd.Next(150);
+            int power = rnd.Next(10);
+            double accurancy = 0.95;
+            double expextedResult = Math.Pow(number, 1.00 / (double)power);
+            double actualResult = NewtonsMethod.RootOfNthDegreeByNewton(number, power, accurancy);
+            double interval = expextedResult * (1 - accurancy);
+            Assert.IsTrue(actualResult >= expextedResult - interval && actualResult <= expextedResult + interval);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", 
+            "|DataDirectory|\\DataRootTest.xml", 
+            "TestCase", DataAccessMethod.Sequential)]
+        [DeploymentItem("RootTest\\DataRootTest.xml")]
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NewtonTestNegative()
+        {
+            double number = Convert.ToDouble(TestContext.DataRow["number"]);
+            int power = Convert.ToInt32(TestContext.DataRow["power"]);
+            double accurancy = Convert.ToDouble(TestContext.DataRow["accurancy"]);
+            NewtonsMethod.RootOfNthDegreeByNewton(number, power, accurancy);
+        }
+
+    }
+}
